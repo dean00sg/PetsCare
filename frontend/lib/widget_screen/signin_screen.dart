a4177c0e637e8dc512ae190cbd3f5304738c8ce0
Darin Kanjanasadung <6410110178@email.psu.ac.th>
2024-09-04 01:59:18 +0700

    add containner for sign up and  Resize botton

--- frontend/lib/widget_screen/signin_screen.dart
+++ frontend/lib/widget_screen/signin_screen.dart
@@ -6,121 +6,140 @@ class SignupScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white, // Light background
+      backgroundColor: const Color.fromARGB(255, 230, 225, 225), // Light background
       body: Center(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              // Centering the image horizontally
-              Image.asset(
-                'lib/images/logo.png',
-                height: 150, // Adjust height as needed
-                width: 150,  // Adjust width as needed
-                fit: BoxFit.contain,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              width: 500,
+              height: 700,
+              margin: const EdgeInsets.symmetric(horizontal: 20.0),
+              padding: const EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 122, 83, 65), // Inner container background color
+                borderRadius: BorderRadius.circular(25),
               ),
-
-              // Adding space between the logo and text fields
-              const SizedBox(height: 30),
-
-              // Text Field for First Name
-              TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.brown[200], // Background color of the text field
-                  hintText: 'FirstName',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-              ),
-
-              const SizedBox(height: 16),
-
-              // Text Field for Last Name
-              TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.brown[200], // Background color of the text field
-                  hintText: 'LastName',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-              ),
-
-              const SizedBox(height: 16),
-
-              // Text Field for Email
-              TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.brown[200], // Background color of the text field
-                  hintText: 'E-mail',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-              ),
-
-              const SizedBox(height: 16),
-
-              // Text Field for Password
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.brown[200], // Background color of the text field
-                  hintText: 'Password',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-              ),
-
-              const SizedBox(height: 16),
-
-              // Text Field for Phone Number
-              TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.brown[200], // Background color of the text field
-                  hintText: 'Phone',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-              ),
-
-              const SizedBox(height: 24),
-
-              // Sign Up Button
-              SizedBox(
-                width: double.infinity, // Full-width button
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.amber, // Button color
-                    padding: const EdgeInsets.symmetric(vertical: 16),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0),
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    const SizedBox(height: 30),
+                    Container(
+                      width: 170, // ขนาดของ Container ใหญ่กว่าขนาดของรูปเล็กน้อยเพื่อเพิ่มขอบ
+                      height: 170,
+                      decoration: const BoxDecoration(
+                        color: Colors.white, 
+                        shape: BoxShape.circle, // ทำให้ Container มีรูปร่างเป็นวงกลม
+                      ),
+                      child: ClipOval(
+                        child: Image.asset(
+                          'lib/images/logo.png',
+                          height: 150, // ขนาดของรูปภาพ
+                          width: 150,
+                          fit: BoxFit.contain,
+                        ),
+                      ),
                     ),
-                  ),
-                  onPressed: () {
-                    // Handle sign up logic here
-                  },
-                  child: const Text('LOGIN', style: TextStyle(fontSize: 16)),
+
+                    const SizedBox(height: 40),
+                    // Text Field for Email
+                    TextField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white, // Background color of the text field
+                        hintText: 'FristName',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                          borderSide: BorderSide.none,
+                        ),
+                      ),
+                    ),
+
+                    const SizedBox(height: 25),
+
+                    // Text Field for Last Name
+                    TextField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white, // Background color of the text field
+                        hintText: 'LastName',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                          borderSide: BorderSide.none,
+                        ),
+                      ),
+                    ),
+
+                    const SizedBox(height: 25),
+
+                    // Text Field for Email
+                    TextField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white, // Background color of the text field
+                        hintText: 'E-mail',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                          borderSide: BorderSide.none,
+                        ),
+                      ),
+                    ),
+
+                    const SizedBox(height: 25),
+
+                    // Text Field for Password
+                    TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white, // Background color of the text field
+                        hintText: 'Password',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                          borderSide: BorderSide.none,
+                        ),
+                      ),
+                    ),
+
+                    const SizedBox(height: 25),
+
+                    // Text Field for Phone Number
+                    TextField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white, // Background color of the text field
+                        hintText: 'Phone',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                          borderSide: BorderSide.none,
+                        ),
+                      ),
+                    ),
+
+                    const SizedBox(height: 30),
+
+                    // Sign Up Button
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.amber, // Button color
+                          padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 50),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(24),
+                          ),
+                      ),
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/');
+                      },
+                      child: const Text('SIGN IN', style: TextStyle(fontSize: 16)),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
-    );
+   );
   }
 }
