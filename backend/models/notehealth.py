530ad5fa0ef71ab961aac3f9a95f516c12717e62
Preawping Eakkarat <preampreawping@gmail.com>
2024-09-09 20:17:42 +0700

    Backend: add notehealth to connect to postgres database

--- backend/models/notehealth.py
+++ backend/models/notehealth.py
@@ -1,35 +1,54 @@
 from datetime import datetime
 from enum import Enum
 from typing import Optional
+from pydantic import BaseModel
 from sqlmodel import SQLModel, Field
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, JSON, Float
+from sqlalchemy.orm import relationship
+from deps import Base
 
-# Define Enum for Pet Types
-class PetType(str, Enum):
-    cat = "cat"
-    dog = "dog"
-    rabbit = "rabbit"
-    fish = "fish"
+class HealthRecord(Base):
+    __tablename__ = 'health_records'
+
+    HR_id = Column(Integer, primary_key=True, index=True)
+    pet_type = Column(String, nullable=False)
+    age_years = Column(Integer, nullable=False)
+    age_months = Column(Integer, nullable=False)
+    age_days = Column(Integer, nullable=False)
+    weight_start_months = Column(Float, nullable=False)
+    weight_end_months = Column(Float, nullable=False)
+
+    record_date = Column(DateTime, default=lambda: datetime.now().replace(microsecond=0)) 
+    description = Column(JSON, nullable=True)
+
+class Age(BaseModel):
+    years: int
+    months: int
+    days: int
+
+    def str(self) -> str:
+        return f"{self.years}y {self.months}m {self.days}d"
 
 
-class PetHealthRecord(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    pet_type: Optional[PetType] = Field(default=None)
-    age: Optional[str] = Field(default=None)  # Format "Xy Xm Xd"
-    weight: Optional[float] = Field(default=None)
-    notes: Optional[str] = Field(default=None)
-    date: datetime = Field(default_factory=datetime.now)
 
-# Pydantic Model for Creating Health Records
-class CreateHealthRecord(SQLModel):
-    pet_type: Optional[PetType] = Field(default=None)
-    date: datetime
-    weight: Optional[float] = Field(default=None)
-    notes: Optional[str] = Field(default=None)
-# class HealthRecord(Base):
-#     __tablename__ = 'health_records'
+class CreateHealthRecord(BaseModel):
+    pet_type: str
+    age: Age
+    weight_start_months: float
+    weight_end_months: float
+    description: Optional[str]
 
-#     id = Column(Integer, primary_key=True, index=True)
-#     pet_id = Column(Integer, ForeignKey('pets.pets_id'))
-#     record_date = Column(DateTime, nullable=False)
-#     description = Column(String, nullable=False)
-#     pet = relationship("Pet", back_populates="health_records")
+    class Config:
+        orm_mode = True
+
+class HealthRecordResponse(BaseModel):
+    HR_id: int
+    pet_type: str
+    age: Age
+    weight_start_months: float
+    weight_end_months: float
+    record_date: datetime
+    description: Optional[str] 
+
+    class Config:
+        orm_mode = True
