e1a7a4dca8152ab9b85565228be6ef3f2b5c8fd5
Darin Kanjanasadung <6410110178@email.psu.ac.th>
2024-09-18 03:48:51 +0700

    Frontend: fix slidebar

--- frontend/lib/users/appbar/slidebar.dart
+++ frontend/lib/users/appbar/slidebar.dart
@@ -6,40 +6,106 @@ class Sidebar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
-        children: <Widget>[
-          const DrawerHeader(
-            decoration: BoxDecoration(
-              color: Colors.brown,
-            ),
-            child: Text(
-              'Menu',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 24,
+      child: Container(
+        color: Colors.white,
+        child: ListView(
+          children: <Widget>[
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.brown,
+              ),
+              child: Text(
+                'Menu',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
               ),
             ),
-          ),
-          ListTile(
-            title: const Text('Feed'),
-            onTap: () {
-              Navigator.pushNamed(context, '/feed');
-            },
-          ),
-          ListTile(
-            title: const Text('PETS'),
-            onTap: () {
-              Navigator.pushNamed(context, '/createpetsmain');
-            },
-          ),
-          ListTile(
-            title: const Text('My Pets'),
-            onTap: () {
-              // Handle tap
-            },
-          ),
-        ],
+            // ใช้ Container ครอบ ListTile แต่ละอัน
+            const SizedBox(height: 25),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 10),
+              decoration: BoxDecoration(
+                color: Colors.brown, 
+                borderRadius: BorderRadius.circular(4),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 5,
+                    offset: const Offset(0, 3),
+                  ),
+                ], 
+              ),
+              child: Column(
+                children: [
+                  ListTile(
+                    title: const Text('Feed', style: TextStyle(color: Colors.white)),
+                    onTap: () {
+                      Navigator.pushNamed(context, '/feed');
+                    },
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 15),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 8.0),
+              decoration: BoxDecoration(
+                color: Colors.brown, 
+                borderRadius: BorderRadius.circular(4),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 5,
+                    offset: const Offset(0, 3),
+                  ),
+                ], 
+              ),
+              child: Column(
+                children: [
+                  ListTile(
+                    title: const Text('PETS', style: TextStyle(color: Colors.white)),
+                    onTap: () {
+                      Navigator.pushNamed(context, '/createpetsmain');
+                    },
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 15),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 8.0),
+              decoration: BoxDecoration(
+                color: Colors.brown, 
+                borderRadius: BorderRadius.circular(4),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 5,
+                    offset: const Offset(0, 3),
+                  ),
+                ], 
+              ),
+              child: Column(
+                children: [
+                  ListTile(
+                    title: const Text('My Pets', style: TextStyle(color: Colors.white)),
+                    onTap: () {
+                      Navigator.pushNamed(context, '/');
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
+
