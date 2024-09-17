1f5f5aede898bf3c825de923764770aa76191bb3
Darin Kanjanasadung <6410110178@email.psu.ac.th>
2024-09-18 05:59:34 +0700

    Frontend: fix create pet style

--- frontend/lib/users/styles/create_pet_style.dart
+++ frontend/lib/users/styles/create_pet_style.dart
@@ -1,29 +1,36 @@
 import 'package:flutter/material.dart';
 
-// สไตล์สำหรับ Container
+// สไตล์ของ Container หลัก
 final BoxDecoration containerDecoration = BoxDecoration(
-  color: const Color.fromARGB(255, 122, 83, 65),
-  borderRadius: BorderRadius.circular(25),
+  color: Colors.brown, // สีน้ำตาลตามรูป
+  borderRadius: BorderRadius.circular(25), // ขอบโค้งมน
 );
 
-// สไตล์ปุ่มบันทึก (Save)
-final ButtonStyle saveButtonStyle = ElevatedButton.styleFrom(
-  backgroundColor: Colors.amber,
-  padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 50),
-  shape: RoundedRectangleBorder(
-    borderRadius: BorderRadius.circular(24),
-  ),
-);
-
-// สไตล์ TextField
+// สไตล์ของ TextField
 InputDecoration inputDecoration(String hintText) {
   return InputDecoration(
     filled: true,
     fillColor: Colors.white,
     hintText: hintText,
     border: OutlineInputBorder(
-      borderRadius: BorderRadius.circular(8.0),
+      borderRadius: BorderRadius.circular(20),
       borderSide: BorderSide.none,
     ),
   );
 }
+
+// สไตล์ของปุ่ม Save
+final ButtonStyle saveButtonStyle = ElevatedButton.styleFrom(
+  backgroundColor: Colors.amber,
+  shape: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(24),
+  ),
+  padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 80),
+);
+
+// สไตล์ของข้อความ "Profile"
+const TextStyle profileTextStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 20,
+  fontWeight: FontWeight.bold,
+);
--- frontend/lib/users/widget_screen/create_pet_screen.dart
+++ frontend/lib/users/widget_screen/create_pet_screen.dart
@@ -4,6 +4,7 @@ import 'package:frontend/users/bloc/create_pet_bloc.dart';
 import 'package:frontend/users/event/create_pet_event.dart';
 import 'package:frontend/users/models/create_pet_model.dart';
 import 'package:frontend/users/state/create_pet_state.dart';
+import 'package:frontend/users/styles/create_pet_style.dart';
 
 class CreatePetScreen extends StatefulWidget {
   const CreatePetScreen({super.key});
@@ -21,13 +22,15 @@ class _CreatePetScreenState extends State<CreatePetScreen> {
   String? _selectedSex;
   String? _imagePath;
 
+  Map<String, dynamic>? args;
+
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
-    final args = ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>?;
+    args = ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>?;
     if (args != null) {
-      _petTypeController.text = args['name'];  // Set type of pet
-      _imagePath = args['imagePath'];
+      _petTypeController.text = args!['name'];  // Set type of pet
+      _imagePath = args!['imagePath'];
     }
   }
 
@@ -86,64 +89,121 @@ class _CreatePetScreenState extends State<CreatePetScreen> {
             );
           }
         },
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: <Widget>[
-              if (_imagePath != null && _imagePath!.isNotEmpty)
-                CircleAvatar(
-                  radius: 50,
-                  backgroundImage: AssetImage(_imagePath!),
-                  backgroundColor: Colors.grey[300],
+        
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'MY ${args != null ? args!['name'].toUpperCase() : ''}',
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.brown[900], 
+                  ),
                 ),
-              const SizedBox(height: 16),
-              TextField(
-                controller: _nameController,
-                decoration: const InputDecoration(labelText: 'Name'),
-              ),
-              TextField(
-                controller: _petTypeController, 
-                decoration: const InputDecoration(labelText: 'Type of Pet'),
-              ),
-              TextField(
-                controller: _dateController,
-                decoration: const InputDecoration(labelText: 'Date of Birth'),
-                readOnly: true,
-                onTap: () => _selectDate(context),
-              ),
-              TextField(
-                controller: _weightController,
-                decoration: const InputDecoration(labelText: 'Weight'),
-                keyboardType: TextInputType.number,
-              ),
-              DropdownButtonFormField<String>(
-                decoration: const InputDecoration(labelText: 'Sex'),
-                value: _selectedSex,
-                items: ['Male', 'Female'].map((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-                onChanged: (String? newValue) {
-                  setState(() {
-                    _selectedSex = newValue;
-                  });
-                },
-              ),
-              TextField(
-                controller: _breedController,
-                decoration: const InputDecoration(labelText: 'Breed'),
-              ),
-              
-              ElevatedButton(
-                onPressed: () => _submitForm(context),
-                child: const Text('Save'),
-              ),
-            ],
+                const SizedBox(height: 16), 
+                // Container สำหรับโปรไฟล์สัตว์เลี้ยง
+                Container(
+                  width: 320,
+                  decoration: containerDecoration, 
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 16),
+                      if (_imagePath != null && _imagePath!.isNotEmpty)
+                        CircleAvatar(
+                          radius: 70,
+                          backgroundImage: AssetImage(_imagePath!),
+                          backgroundColor: Colors.grey[300],
+                        ),
+                      const SizedBox(height: 16),
+
+                      const Text(
+                        'PROFILE',
+                        style: profileTextStyle, 
+                      ),
+                      const SizedBox(height: 16),
+
+                      // ชื่อสัตว์เลี้ยง
+                      TextField(
+                        controller: _nameController,
+                        decoration: inputDecoration('Name'),
+                      ),
+                      const SizedBox(height: 16),
+
+                      // ประเภทของสัตว์เลี้ยง
+                      TextField(
+                        controller: _petTypeController, 
+                        decoration: inputDecoration('Type of Pet'), 
+                      ),
+                      const SizedBox(height: 16),
+
+                      // วันเกิดของสัตว์เลี้ยง
+                      TextField(
+                        controller: _dateController,
+                        decoration: inputDecoration('Date of Birth'), 
+                        readOnly: true,
+                        onTap: () => _selectDate(context),
+                      ),
+                      const SizedBox(height: 16),
+
+                      // น้ำหนักของสัตว์เลี้ยง
+                      TextField(
+                        controller: _weightController,
+                        decoration: inputDecoration('Weight'), 
+                        keyboardType: TextInputType.number,
+                      ),
+                      const SizedBox(height: 16),
+
+                      // เพศของสัตว์เลี้ยง
+                      DropdownButtonFormField<String>(
+                        decoration: inputDecoration('Sex'), 
+                        value: _selectedSex,
+                        items: ['Male', 'Female'].map((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            _selectedSex = newValue;
+                          });
+                        },
+                      ),
+                      const SizedBox(height: 16),
+
+                      // สายพันธุ์ของสัตว์เลี้ยง
+                      TextField(
+                        controller: _breedController,
+                        decoration: inputDecoration('Breed'), 
+                      ),
+                      const SizedBox(height: 20),
+
+                      // ปุ่ม Save
+                      SizedBox(
+                        width: double.infinity, 
+                        child: ElevatedButton(
+                          style: saveButtonStyle, 
+                          onPressed: () => _submitForm(context),
+                          child: const Text(
+                            'Save',
+                            style: TextStyle(fontSize: 16, color: Colors.black),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
+
