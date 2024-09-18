30afa4fcc5caedef5ac7492f6692bc898bc4537b
Darin Kanjanasadung <6410110178@email.psu.ac.th>
2024-09-18 22:36:31 +0700

    Frontend: fix slidebar

--- frontend/lib/users/appbar/slidebar.dart
+++ frontend/lib/users/appbar/slidebar.dart
@@ -1,124 +1,7 @@
-
-
-// import 'package:flutter/material.dart';
-
-// class Sidebar extends StatelessWidget {
-//   const Sidebar({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Drawer(
-//       child: Container(
-//         color: Colors.white,
-//         child: ListView(
-//           children: <Widget>[
-//             const DrawerHeader(
-//               decoration: BoxDecoration(
-//                 color: Colors.brown,
-//               ),
-//               child: Text(
-//                 'Menu',
-//                 style: TextStyle(
-//                   color: Colors.white,
-//                   fontSize: 24,
-//                 ),
-//               ),
-//             ),
-//             // ใช้ Container ครอบ ListTile แต่ละอัน
-//             const SizedBox(height: 25),
-//             Container(
-//               margin: const EdgeInsets.symmetric(horizontal: 10),
-//               decoration: BoxDecoration(
-//                 color: Colors.brown, 
-//                 borderRadius: BorderRadius.circular(4),
-//                 boxShadow: [
-//                   BoxShadow(
-//                     color: Colors.grey.withOpacity(0.5),
-//                     spreadRadius: 2,
-//                     blurRadius: 5,
-//                     offset: const Offset(0, 3),
-//                   ),
-//                 ], 
-//               ),
-//               child: Column(
-//                 children: [
-//                   ListTile(
-//                     title: const Text('Feed', style: TextStyle(color: Colors.white)),
-//                     onTap: () {
-//                       Navigator.pushNamed(context, '/feed');
-//                     },
-//                   ),
-//                 ],
-//               ),
-//             ),
-//             const SizedBox(height: 15),
-//             Container(
-//               margin: const EdgeInsets.symmetric(horizontal: 8.0),
-//               decoration: BoxDecoration(
-//                 color: Colors.brown, 
-//                 borderRadius: BorderRadius.circular(4),
-//                 boxShadow: [
-//                   BoxShadow(
-//                     color: Colors.grey.withOpacity(0.5),
-//                     spreadRadius: 2,
-//                     blurRadius: 5,
-//                     offset: const Offset(0, 3),
-//                   ),
-//                 ], 
-//               ),
-//               child: Column(
-//                 children: [
-//                   ListTile(
-//                     title: const Text('PETS', style: TextStyle(color: Colors.white)),
-//                     onTap: () {
-//                       Navigator.pushNamed(context, '/createpetsmain');
-//                     },
-//                   ),
-//                 ],
-//               ),
-//             ),
-//             const SizedBox(height: 15),
-//             Container(
-//               margin: const EdgeInsets.symmetric(horizontal: 8.0),
-//               decoration: BoxDecoration(
-//                 color: Colors.brown, 
-//                 borderRadius: BorderRadius.circular(4),
-//                 boxShadow: [
-//                   BoxShadow(
-//                     color: Colors.grey.withOpacity(0.5),
-//                     spreadRadius: 2,
-//                     blurRadius: 5,
-//                     offset: const Offset(0, 3),
-//                   ),
-//                 ], 
-//               ),
-//               child: Column(
-//                 children: [
-//                   ListTile(
-//                     title: const Text('My Pets', style: TextStyle(color: Colors.white)),
-//                     onTap: () {
-//                       Navigator.pushNamed(context, '/');
-//                     },
-//                   ),
-//                 ],
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-
-
-//โค้ดด้านล่างจะโชว์สัตว์เลี้ยงของตัวเอง  ข้างบน จะเป็นโค้ดเก่า
-
-
-
 import 'package:flutter/material.dart';
 import 'package:frontend/users/models/pet_models.dart';
 import 'package:frontend/users/repositories/pets_repository.dart';
+
 class Sidebar extends StatefulWidget {
   const Sidebar({super.key});
 
@@ -138,6 +21,7 @@ class _SidebarState extends State<Sidebar> {
 
   @override
   Widget build(BuildContext context) {
+    final currentRoute = ModalRoute.of(context)?.settings.name;
     return Drawer(
       child: Container(
         color: Colors.white,
@@ -156,65 +40,11 @@ class _SidebarState extends State<Sidebar> {
               ),
             ),
             const SizedBox(height: 25),
-            _buildFeedTile(),
-            const SizedBox(height: 15),
-            _buildPetsList(),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildFeedTile() {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 10),
-      decoration: BoxDecoration(
-        color: Colors.brown,
-        borderRadius: BorderRadius.circular(4),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.5),
-            spreadRadius: 2,
-            blurRadius: 5,
-            offset: const Offset(0, 3),
-          ),
-        ],
-      ),
-      child: Column(
-        children: [
-          ListTile(
-            title: const Text('Feed', style: TextStyle(color: Colors.white)),
-            onTap: () {
-              Navigator.pushNamed(context, '/feed');
-            },
-          ),
-        ],
-        
-      ),
-      
-    );
-  }
-
-  Widget _buildPetsList() {
-    return FutureBuilder<List<Pet>>(
-      future: futurePets,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(child: CircularProgressIndicator()); // Loading indicator
-        } else if (snapshot.hasError) {
-          return Text('Error: ${snapshot.error}');
-        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-          return const Center(child: Text('No pets found'));
-        }
-
-        final pets = snapshot.data!;
-
-        return Column(
-          children: pets.map((pet) {
-            return Container(
-              margin: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 5.0),
+            if (currentRoute != '/feed') 
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 10),
               decoration: BoxDecoration(
-                color: Colors.brown,
+                color: Colors.brown, 
                 borderRadius: BorderRadius.circular(4),
                 boxShadow: [
                   BoxShadow(
@@ -223,19 +53,114 @@ class _SidebarState extends State<Sidebar> {
                     blurRadius: 5,
                     offset: const Offset(0, 3),
                   ),
+                ], 
+              ),
+              child: Column(
+                children: [
+                  ListTile(
+                    title: const Text('HOME', style: TextStyle(color: Colors.white)),
+                    onTap: () {
+                      Navigator.pushNamed(context, '/feed');
+                    },
+                  ),
                 ],
               ),
-              child: ListTile(
-                leading: Image.asset(pet.imagePath), // Display pet image from assets
-                title: Text(pet.name, style: const TextStyle(color: Colors.white)),
-                onTap: () {
-                  // Handle navigation or actions for each pet
-                },
+            ),
+            const SizedBox(height: 15),
+            if (currentRoute != '/createpetsmain')
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 8.0),
+              decoration: BoxDecoration(
+                color: Colors.brown, 
+                borderRadius: BorderRadius.circular(4),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 5,
+                    offset: const Offset(0, 3),
+                  ),
+                ], 
               ),
-            );
-          }).toList(),
-        );
-      },
+              child: Column(
+                children: [
+                  ListTile(
+                    title: const Text('TYPE PETS', style: TextStyle(color: Colors.white)),
+                    onTap: () {
+                      Navigator.pushNamed(context, '/createpetsmain');
+                    },
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 15),
+
+            // MY PETS Container with ExpansionTile
+            FutureBuilder<List<Pet>>(
+              future: futurePets,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                  return const Center(child: Text('No pets found', style: TextStyle(color: Colors.brown)));
+                }
+
+                final pets = snapshot.data!;
+
+                return Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 8.0),
+                  decoration: BoxDecoration(
+                    color: Colors.brown,
+                    borderRadius: BorderRadius.circular(4),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                        offset: const Offset(0, 3),
+                      ),
+                    ],
+                  ),
+                  child: ExpansionTile(
+                    title: const Text(
+                      'MY PETS',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    iconColor: Colors.white,
+                    collapsedIconColor: Colors.white,
+                    children: pets.map((pet) {
+                      return Container(
+                        margin: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
+                        padding: const EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(
+                          color: Colors.brown[50], 
+                          borderRadius: BorderRadius.circular(4),
+                        ),
+                        child: Row(
+                          children: [
+                            Image.asset(
+                              pet.imagePath,
+                              width: 30,
+                              height: 30,
+                            ),
+                            const SizedBox(width: 10),
+                            Text(
+                              pet.name,
+                              style: const TextStyle(color: Colors.brown),
+                            ),
+                          ],
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
