f221759237c695e8e134143e362303bbfcf09215
Dean_sg001 <burhanurdin.sg@gmail.com>
2024-09-19 09:56:51 +0700

    Frontend: update locohost

--- frontend/lib/users/widget_screen/profile.dart
+++ frontend/lib/users/widget_screen/profile.dart
@@ -126,7 +126,7 @@ class ProfileScreen extends StatelessWidget {
                       child: OutlinedButton(
                         style: logoutButtonStyle,
                         onPressed: () async {
-                          final profileRepository = ProfileRepository(apiUrl: 'http://127.0.0.1:8000');
+                          final profileRepository = ProfileRepository(apiUrl: 'http://10.0.2.2:8000');
                           await profileRepository.logout();
 
                           // Navigate to the login screen or another appropriate screen
