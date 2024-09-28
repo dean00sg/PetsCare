fb46fe5da2756d605897fee9741914b7c4d1d023
Darin Kanjanasadung <6410110178@email.psu.ac.th>
2024-09-29 00:58:31 +0700

    Frontend: add history record style

--- frontend/lib/admin/widget/add_historyrec.dart
+++ frontend/lib/admin/widget/add_historyrec.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:frontend/admin/bloc/add_historyrec.dart'; // History record bloc
-import 'package:frontend/admin/event/add_historyrec.dart'; // History record event
-import 'package:frontend/admin/models/historyrec.dart'; // History record model
+import 'package:frontend/admin/bloc/add_historyrec.dart';
+import 'package:frontend/admin/event/add_historyrec.dart';
+import 'package:frontend/admin/models/historyrec.dart';
 import 'package:frontend/admin/models/petprofileuser.dart';
 import 'package:frontend/admin/repositories/petprofileuser.dart';
-import 'package:frontend/admin/state/add_historyrec.dart'; // History record state
+import 'package:frontend/admin/state/add_historyrec.dart';
+import 'package:frontend/admin/style/add_vaccination_style.dart';
+import 'package:frontend/users/bloc/profile_bloc.dart';
+import 'package:frontend/users/state/profile_state.dart'; 
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
 
 class AddHistoryRecScreen extends StatefulWidget {
   const AddHistoryRecScreen({super.key});
@@ -20,7 +24,8 @@ class _AddHistoryRecScreenState extends State<AddHistoryRecScreen> {
   final symptomsController = TextEditingController();
   final diagnoseController = TextEditingController();
   final remarkController = TextEditingController();
-  
+  final TextEditingController _typeAheadController = TextEditingController();
+
   // Pet and Owner dropdown states
   List<PetProfileUserModel> petProfiles = [];
   List<String> ownerNames = [];
@@ -34,7 +39,7 @@ class _AddHistoryRecScreenState extends State<AddHistoryRecScreen> {
   void initState() {
     super.initState();
     _loadPetProfiles();
-    _loadUsername(); // Load username when initializing the screen
+    _loadUsername();
   }
 
   Future<void> _loadPetProfiles() async {
@@ -43,7 +48,7 @@ class _AddHistoryRecScreenState extends State<AddHistoryRecScreen> {
 
     setState(() {
       petProfiles = profiles;
-      ownerNames = profiles.map((pet) => pet.owner_name).toSet().toList(); // Get unique owner names
+      ownerNames = profiles.map((pet) => pet.owner_name).toSet().toList();
     });
   }
 
@@ -61,7 +66,7 @@ class _AddHistoryRecScreenState extends State<AddHistoryRecScreen> {
           .where((pet) => pet.owner_name == selectedOwner)
           .map((pet) => pet.name)
           .toList();
-      selectedPet = null; // Reset selected pet when owner changes
+      selectedPet = null; 
     });
   }
 
@@ -70,9 +75,7 @@ class _AddHistoryRecScreenState extends State<AddHistoryRecScreen> {
     return BlocListener<AddHistoryRecBloc, AddHistoryRecState>(
       listener: (context, state) {
         if (state is AddHistoryRecSuccess) {
-          ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(content: Text('History record added successfully')),
-          );
+          ScaffoldMessenger.of(context).showSnackBar(snackBarStyle);
           Navigator.pop(context); // Navigate back after success
         } else if (state is AddHistoryRecFailure) {
           ScaffoldMessenger.of(context).showSnackBar(
@@ -82,82 +85,214 @@ class _AddHistoryRecScreenState extends State<AddHistoryRecScreen> {
       },
       child: Scaffold(
         appBar: AppBar(
-          title: const Text('Add History Record'),
+          iconTheme: const IconThemeData(color: Colors.white),
+          title: const Text("Pet History Record",
+              style: TextStyle(fontSize: 22, color: Colors.white)),
+          backgroundColor: const Color(0xFF266FCA),
+          centerTitle: true,
+          toolbarHeight: 70,
         ),
         body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              // Owner Name Dropdown
-              DropdownButton<String>(
-                value: selectedOwner,
-                hint: const Text('Select Owner'),
-                onChanged: _onOwnerChanged,
-                items: ownerNames.map<DropdownMenuItem<String>>((String owner) {
-                  return DropdownMenuItem<String>(
-                    value: owner,
-                    child: Text(owner),
-                  );
-                }).toList(),
-              ),
-              
-              // Pet Name Dropdown
-              DropdownButton<String>(
-                value: selectedPet,
-                hint: const Text('Select Pet'),
-                onChanged: (newValue) {
-                  setState(() {
-                    selectedPet = newValue;
-                  });
-                },
-                items: petNames.map<DropdownMenuItem<String>>((String pet) {
-                  return DropdownMenuItem<String>(
-                    value: pet,
-                    child: Text(pet),
-                  );
-                }).toList(),
-              ),
-              
-              TextField(
-                controller: headerController,
-                decoration: const InputDecoration(labelText: 'Record Header'),
-              ),
-              TextField(
-                controller: symptomsController,
-                decoration: const InputDecoration(labelText: 'Symptoms'),
-              ),
-              TextField(
-                controller: diagnoseController,
-                decoration: const InputDecoration(labelText: 'Diagnose'),
-              ),
-              TextField(
-                controller: remarkController,
-                decoration: const InputDecoration(labelText: 'Remark'),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  if (selectedOwner != null && selectedPet != null && username != null) {
-                    final record = AddHistoryRec(
-                      header: headerController.text,
-                      symptoms: symptomsController.text,
-                      diagnose: diagnoseController.text,
-                      remark: remarkController.text,
-                      petName: selectedPet!,
-                      ownerName: selectedOwner!,
-                    );
+          padding: const EdgeInsets.all(16),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 4),
+                Text(
+                  'Add History Record',
+                  style: titleTextStyle,
+                ),
+                const SizedBox(height: 12),
+                BlocBuilder<ProfileBloc, ProfileState>(
+                  builder: (context, state) {
+                    if (state is ProfileLoaded) {
+                      return Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const SizedBox(height: 12),
+                          Container(
+                            padding: const EdgeInsets.all(16.0),
+                            decoration: containerHistoryBoxDecoration,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Container(
+                                  padding: const EdgeInsets.all(16.0),
+                                  decoration: profileContainerBoxDecoration,
+                                  child: Row(
+                                    children: [
+                                      CircleAvatar(
+                                        backgroundImage: state.profile.imageUrl != null
+                                            ? NetworkImage(state.profile.imageUrl!)
+                                            : null,
+                                        radius: 30,
+                                        child: state.profile.imageUrl == null
+                                            ? const Icon(Icons.person, size: 30)
+                                            : null,
+                                      ),
+                                      const SizedBox(width: 12),
+                                      Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            "${state.profile.firstName} ${state.profile.lastName}",
+                                            style: const TextStyle(
+                                              fontSize: 20,
+                                              fontWeight: FontWeight.bold,
+                                              color: Colors.white,
+                                            ),
+                                          ),
+                                          Text(state.profile.email,
+                                              style: const TextStyle(color: Colors.white)),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                const SizedBox(height: 8),
+                                const Text('Owner:', style: TextStyle(color: Colors.white)),
+                                const SizedBox(height: 5),
+                                Row(
+                                  children: [
+                                    Expanded(
+                                      flex: 3,
+                                      child: TypeAheadFormField<String>(
+                                        textFieldConfiguration: TextFieldConfiguration(
+                                          controller: _typeAheadController,
+                                          decoration: InputDecoration(
+                                            filled: true,
+                                            fillColor: Colors.white,
+                                            border: OutlineInputBorder(
+                                              borderRadius: BorderRadius.circular(10.0),
+                                              borderSide: BorderSide.none,
+                                            ),
+                                            suffixIcon: const Icon(Icons.search, color: Colors.blue,),
+                                          ),
+                                        ),
+                                        suggestionsCallback: (pattern) {
+                                          return ownerNames.where((owner) => owner
+                                              .toLowerCase()
+                                              .contains(pattern.toLowerCase()));
+                                        },
+                                        itemBuilder: (context, String suggestion) {
+                                          return ListTile(
+                                            title: Text(suggestion),
+                                          );
+                                        },
+                                        onSuggestionSelected: (String suggestion) {
+                                          setState(() {
+                                            selectedOwner = suggestion;
+                                            _typeAheadController.text = suggestion;
+                                          });
+                                          _onOwnerChanged(suggestion);
+                                        },
+                                        noItemsFoundBuilder: (context) =>
+                                            const Padding(
+                                          padding: EdgeInsets.all(8.0),
+                                          child: Text('No owners found'),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(height: 10),
+                                const Text('Pet Name:', style: TextStyle(color: Colors.white)),
+                                const SizedBox(height: 5),
+                                Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(10.0),
+                                  ),
+                                  padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                                  child: DropdownButton<String>(
+                                    value: selectedPet,
+                                    hint: const Text('Select Pet', style: TextStyle(color: Colors.white)),
+                                    onChanged: (newValue) {
+                                      setState(() {
+                                        selectedPet = newValue;
+                                      });
+                                    },
+                                    isExpanded: true,
+                                    underline: const SizedBox(),
+                                    items: petNames.map<DropdownMenuItem<String>>((String pet) {
+                                      return DropdownMenuItem<String>(
+                                        value: pet,
+                                        child: Text(pet),
+                                      );
+                                    }).toList(),
+                                  ),
+                                ),
+                                const SizedBox(height: 10),
+                                const Text('Record Header:', style: TextStyle(color: Colors.white)),
+                                const SizedBox(height: 5),
+                                TextField(
+                                  controller: headerController,
+                                  decoration: inputDecorationStyle,
+                                ),
+                                const SizedBox(height: 10),
+                                const Text('Symptoms:', style: TextStyle(color: Colors.white)),
+                                const SizedBox(height: 5),
+                                TextField(
+                                  controller: symptomsController,
+                                  decoration: inputDecorationStyle,
+                                ),
+                                const SizedBox(height: 10),
+                                const Text('Diagnose:', style: TextStyle(color: Colors.white)),
+                                const SizedBox(height: 5),
+                                TextField(
+                                  controller: diagnoseController,
+                                  decoration: inputDecorationStyle,
+                                ),
+                                const SizedBox(height: 10),
+                                const Text('Remark: Optional', style: TextStyle(color: Colors.white)),
+                                const SizedBox(height: 5),
+                                TextField(
+                                  controller: remarkController,
+                                  decoration: inputDecorationStyle,
+                                ),
+                                const SizedBox(height: 30),
+                                SizedBox(
+                                  width: double.infinity,
+                                  child: ElevatedButton(
+                                    style: submitButtonStyle,
+                                    onPressed: () {
+                                      if (selectedOwner != null &&
+                                          selectedPet != null &&
+                                          username != null) {
+                                        final record = AddHistoryRec(
+                                          header: headerController.text,
+                                          symptoms: symptomsController.text,
+                                          diagnose: diagnoseController.text,
+                                          remark: remarkController.text,
+                                          petName: selectedPet!,
+                                          ownerName: selectedOwner!,
+                                        );
 
-                    // Dispatch the event to the bloc
-                    context.read<AddHistoryRecBloc>().add(SubmitHistoryRecForm(record));
-                  } else {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text('Please fill out all fields')),
-                    );
-                  }
-                },
-                child: const Text('Submit'),
-              ),
-            ],
+                                        // Dispatch the event to the bloc
+                                        context.read<AddHistoryRecBloc>().add(SubmitHistoryRecForm(record));
+                                      } else {
+                                        ScaffoldMessenger.of(context).showSnackBar(
+                                          const SnackBar(content: Text('Please fill out all fields')),
+                                        );
+                                      }
+                                    },
+                                    child: const Text('Save',
+                                        style: TextStyle(color: Colors.white, fontSize: 16)),
+                                  ),
+                                ),
+                                const SizedBox(height: 10),
+                              ],
+                            ),
+                          ),
+                        ],
+                      );
+                    }
+                    return const SizedBox();
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
