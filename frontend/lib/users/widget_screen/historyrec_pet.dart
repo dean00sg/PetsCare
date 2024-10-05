a0d26dcec0f3ef6497ead1cdfaeae1d5dd3a0d15
Preawping Eakkarat <preampreawping@gmail.com>
2024-10-05 22:21:27 +0700

    Frontend: add history record pet style

--- frontend/lib/users/widget_screen/historyrec_pet.dart
+++ frontend/lib/users/widget_screen/historyrec_pet.dart
@@ -3,65 +3,243 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:frontend/users/bloc/historyrec_pet.dart';
 import 'package:frontend/users/event/historyrec_pet.dart';
 import 'package:frontend/users/state/historyrec_pet.dart';
+import 'package:frontend/users/styles/petprofile_style.dart';
+import 'package:intl/intl.dart';
 
 class HistoryRecUserScreen extends StatelessWidget {
   const HistoryRecUserScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final arguments = ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>;
+    final arguments =
+        ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>;
     final petsId = arguments['petsId'] as int;
     final petName = arguments['name'] as String;
     final imagePath = arguments['image'] as String;
 
-    // Dispatch event to load history records
-    context.read<HistoryRecUserBloc>().add(LoadHistoryRecUser(petsId.toString()));
+    context
+        .read<HistoryRecUserBloc>()
+        .add(LoadHistoryRecUser(petsId.toString()));
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('History of $petName'),
+        iconTheme: const IconThemeData(color: Colors.white),
+        backgroundColor: PetProfileStyles.appBarBackgroundColor,
+        centerTitle: true,
+        toolbarHeight: 70,
+        title: const Text(
+          'History Record',
+          style: TextStyle(fontSize: 22, color: Colors.white),
+        ),
       ),
-      body: BlocBuilder<HistoryRecUserBloc, HistoryRecUserState>(
-        builder: (context, state) {
-          if (state is HistoryRecUserLoading) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (state is HistoryRecUserLoaded) {
-            final records = state.records;
-            return ListView.builder(
-              itemCount: records.length,
-              itemBuilder: (context, index) {
-                final record = records[index];
-                return Card(
-                  margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      backgroundImage: NetworkImage(imagePath), // Displaying the image
-                      radius: 30, // Adjust the radius as needed
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Padding(
+              padding: EdgeInsets.fromLTRB(
+                MediaQuery.of(context).size.width * 0.05,
+                16.0,
+                MediaQuery.of(context).size.width * 0.05,
+                0.0,
+              ),
+              // Pet Name Container
+              child: Container(
+                width: double.infinity,
+                color: const Color(0xFF41A785),
+                height: 80,
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 16.0, vertical: 16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    CircleAvatar(
+                      // Pet Profile Image
+                      radius: 25,
+                      backgroundImage: AssetImage(imagePath),
                     ),
-                    title: Text(record.header),
-                    subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('Pet Name: ${record.petName}'),
-                        Text('Owner: ${record.ownerName}'),
-                        Text('Symptoms: ${record.symptoms}'),
-                        Text('Diagnose: ${record.diagnose}'),
-                        Text('Remark: ${record.remark}'),
-                        Text('Note By: ${record.noteBy}'),
-                        Text('Note Name: ${record.noteName}'),
-                        Text('Date: ${record.recordDateTime.toString()}'),
-                      ],
+                    Text(
+                      ' $petName',
+                      style: const TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
                     ),
-                  ),
-                );
-              },
-            );
-          } else if (state is HistoryRecUserError) {
-            return Center(child: Text('Error: ${state.message}'));
-          } else {
-            return const Center(child: Text('No records found.'));
-          }
-        },
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(
+                MediaQuery.of(context).size.width * 0.05,
+                0.0,
+                MediaQuery.of(context).size.width * 0.05,
+                16.0,
+              ),
+              child: Container(
+                width: double.infinity,
+                color: const Color(0xFF98D9B8), // Main container for history
+                child: BlocBuilder<HistoryRecUserBloc, HistoryRecUserState>(
+                  builder: (context, state) {
+                    if (state is HistoryRecUserLoading) {
+                      return const Center(child: CircularProgressIndicator());
+                    } else if (state is HistoryRecUserError) {
+                      return Center(child: Text('Error: ${state.message}'));
+                    } else if (state is HistoryRecUserLoaded) {
+                      return Column(
+                        children: [
+                          ListView.builder(
+                            shrinkWrap: true,
+                            physics: const NeverScrollableScrollPhysics(),
+                            itemCount: state.records.length,
+                            itemBuilder: (context, index) {
+                              final record = state.records[index];
+                              return _buildHistoryContainer(record);
+                            },
+                          ),
+                        ],
+                      );
+                    } else {
+                      return const Center(child: Text('No records found.'));
+                    }
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildHistoryContainer(record) {
+    final formattedDate = DateFormat('yyyy-MM-dd')
+        .format(record.recordDateTime);
+
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+      decoration: BoxDecoration(
+        color: Colors.white, //Container of history information
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.3), 
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          //Date Container
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+            decoration: const BoxDecoration(
+              color: Color.fromARGB(255, 9, 154, 132), 
+            ),
+            child: Text(
+              'Date: $formattedDate',
+              style: const TextStyle(
+                fontSize: 16,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          //Note Name Container
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+            decoration: const BoxDecoration(
+              color: Color.fromARGB(255, 100, 150, 230), 
+            ),
+            child: Text(
+              'Note Name: ${record.noteName}',
+              style: const TextStyle(
+                fontSize: 16,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          //Main information container 
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 16),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Header: ',
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    Expanded(
+                      child: Text(
+                        record.header,
+                        style: const TextStyle(fontSize: 16),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 8),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Symptoms: ',
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    Expanded(
+                      child: Text(
+                        record.symptoms,
+                        style: const TextStyle(fontSize: 16),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 8),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Diagnose: ',
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    Expanded(
+                      child: Text(
+                        record.diagnose,
+                        style: const TextStyle(fontSize: 16),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 8),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Remark: ',
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    Expanded(
+                      child: Text(
+                        record.remark,
+                        style: const TextStyle(fontSize: 16),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
