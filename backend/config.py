e825c94a58eda5983e56db297d93e4515dede014
Preawping Eakkarat <preampreawping@gmail.com>
2024-10-15 04:36:01 +0700

    move dockerfile

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = 'postgresql://postgres:010203@localhost:5432/petscare_db'
+    DATABASE_URL: str = 'postgresql://postgres:postgres@34.124.181.238:5432/petcare_db'
     SECRET_KEY: str = "254fg4r845FESF5A87wewa5rg51h5s4a51g51e4a8t5g"
 
     class Config:
